b68f3e2420da60fdd0781d9baba92dedd60af621
yellowellaphant <ellaupchurch11@gmail.com>
2024-07-29 20:34:27 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          Ella Upchurch
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/yellowellaphant"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          yellowellaphant (GitHub)
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
