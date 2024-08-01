f0908d6fbe1532b01c3650fe315df92121cf5588
yellowellaphant <ellaupchurch11@gmail.com>
2024-07-31 21:08:53 -0500

    Adding under construction screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
+        <UnderConstruction />
+        {/*<p>
           Ella Upchurch
         </p>        
         <a
@@ -17,7 +18,7 @@ function App() {
         >
           yellowellaphant (GitHub)
         </a>
-        <Minigame />
+        <Minigame />*/}
       </header>
     </div>
   );
@@ -25,6 +26,13 @@ function App() {
 
 function Minigame() {
   return <div className='minigame-div'>minigame</div>;
+}
+
+function UnderConstruction() {
+  return <div className='under-construction'>    
+    <img className='construction-cat-png' src="https://cdn.sanity.io/images/2ctnu6ou/production/b6c798156689e8d65c3baa04595e22b169871bfc-1440x1080.png"/>
+    <h1>Under Construction</h1>
+  </div>
 }
 
 export default App;
