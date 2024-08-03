53529e246a7756f65496d23c066927f2eb6b7b7f
yellowellaphant <ellaupchurch11@gmail.com>
2024-08-03 12:38:09 -0500

    Adding "under construction" cat picture & text

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
+        <UnderConstruction />
       </header>
     </div>
   );
+}
+
+function UnderConstruction() {
+  return <div className='under-construction'>    
+    <img className='construction-cat-png' src="https://cdn.sanity.io/images/2ctnu6ou/production/b6c798156689e8d65c3baa04595e22b169871bfc-1440x1080.png"/>
+    <h1>Under Construction</h1>
+  </div>
 }
 
 export default App;
