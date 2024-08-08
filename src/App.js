2d66189d4eaf8effd2305238c6d9c6156694064d
yellowellaphant <ellaupchurch11@gmail.com>
2024-08-07 21:38:25 -0500

    Created VERY basic layout for website

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-                {/*<a
+      <header className="App-header">        
+        <Header />
+        <p>
+          Ella Upchurch
+        </p>        
+        <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/yellowellaphant"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>*/}
-        <UnderConstruction />
+          yellowellaphant (GitHub)
+        </a>        
       </header>
+      <Section />
+      <Minigame />
     </div>
   );
+}
+
+function Minigame() {
+  return <div className='minigame-div'>minigame</div>;
 }
 
 function UnderConstruction() {
@@ -25,6 +33,27 @@ function UnderConstruction() {
     <img className='construction-cat-png' src="https://cdn.sanity.io/images/2ctnu6ou/production/b6c798156689e8d65c3baa04595e22b169871bfc-1440x1080.png"/>
     <h1>Under Construction</h1>
   </div>
+}
+
+function Header(){
+  return(
+  <header id='main-nav'>
+    <nav>
+      <a id='main-nav_home' className='main-nav_link'>Home</a>
+      <a id='main-nav_resume' className='main-nav_link'>Resume</a>
+      <a id='main-nav_contact' className='main-nav_link'>Contact</a>
+      <a id='main-nav_fun' className='main-nav_link'>Fun</a>
+    </nav>    
+  </header>);
+}
+
+function Section(){
+  return(
+  <div>
+  <section>
+    <p>paragraph</p>
+  </section>
+  </div>);
 }
 
 export default App;
