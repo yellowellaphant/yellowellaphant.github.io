0403eba8f36b18498b40128da8381ed90a102825
yellowellaphant <ellaupchurch11@gmail.com>
2025-05-18 18:59:52 -0500

    adjusted fonts

--- src/Components/AirforceSection.js
+++ src/Components/AirforceSection.js
@@ -3,6 +3,9 @@ import '../styles.css';
 function AirforceSection() {
     return (
     <div className="airforce-block block">
+        <div>
+            <h3>Served 6 Years In The</h3>
+        </div>
         <div className="airforce-logo-wrapper">
             <img id="airforce-logo-filled" className="airforce-logo" src="/AF-Filled.png" alt="filled U.S. Air Force Logo"/>
             <img id="airforce-logo-outline" className="airforce-logo" src="/AF-Outline.png" alt="sketched ouline of a U.S. Air Force Logo"/>
