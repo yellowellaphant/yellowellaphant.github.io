ae7877ba351cd9ffc06f55742c3641834032fcf1
yellowellaphant <ellaupchurch11@gmail.com>
2025-06-07 16:20:56 -0500

    added real profile photo

--- src/Components/AirforceSection.js
+++ src/Components/AirforceSection.js
@@ -7,8 +7,12 @@ function AirforceSection() {
             <h3>SERVED 6 YEARS IN THE</h3>
         </div>
         <div className="airforce-logo-wrapper">
+            <img id="placeholder-af-logo" aria-hidden="true" className="airforce-logo" src="/AF-Filled.png" alt=""/>
             <img id="airforce-logo-filled" className="airforce-logo" src="/AF-Filled.png" alt="filled United States Air Force Logo"/>
             <img id="airforce-logo-outline" className="airforce-logo" src="/AF-Outline.png" alt="sketched outline of a United States Air Force Logo"/>
+        </div>
+        <div id="under-af-logo-text">
+            <h3>U.S. AIR FORCE</h3>
         </div>
     </div>
 );
--- src/Components/ProfilePhotoSection.js
+++ src/Components/ProfilePhotoSection.js
@@ -2,7 +2,7 @@ function ProfilePhotoSection() {
     return (
     <div className="profile-photo block">
         <div className="profile-image-wrapper">
-            <img src="/profile-sketch-outline.png" className="profile-image" id="profile-image-outline"></img>
+            <img src="/profile-sketch-real.png" className="profile-image" id="profile-image-outline"></img>
             <img src="/profile-sketch-color.png" className="profile-image" id="profile-image-fill"></img>
         </div>
     </div>
